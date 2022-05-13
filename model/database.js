a53571c3493f40089995f53d86b536e6377d9f7c
MariaBia-Maclagan <82051716+MariaBia-Maclagan@users.noreply.github.com>
2022-05-13 09:29:34 +0100

    routers

--- model/database.js
+++ model/database.js
@@ -10,7 +10,7 @@ const con = mysql.createConnection({
   host: DB_HOST || "127.0.0.1",
   user: DB_USER || "root",
   password: DB_PASS,
-  database: DB_NAME || "todos",
+  database: DB_NAME || "hours",
   multipleStatements: true
 });
 
@@ -18,10 +18,10 @@ con.connect(function(err) {
   if (err) throw err;
   console.log("Connected!");
 
-  let sql = "DROP TABLE if exists items; CREATE TABLE items(id INT NOT NULL AUTO_INCREMENT, text VARCHAR(40) not null, complete BOOLEAN, PRIMARY KEY (id));";
+  let sql = "DROP TABLE if exists schedule; CREATE TABLE schedule(id INT NOT NULL AUTO_INCREMENT, week INT not null, employee VARCHAR(40) not null, start VARCHAR(20) not null, finish VARCHAR(20) not null, hour DECIMAL(19, 2) not null, day VARCHAR(40) not null, date VARCHAR(40) not null, PRIMARY KEY (id));";
   con.query(sql, function (err, result) {
     if (err) throw err;
-    console.log("Table creation `items` was successful!");
+    console.log("Table creation `schedule` was successful!");
 
     console.log("Closing...");
   });
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,56 @@
 var express = require('express');
 var router = express.Router();
+const db = require("../model/helper");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.send("Welcome");
 });
 
+router.get("/hours", (req, res) =>{
+  db("SELECT * FROM schedule")
+  .then(results =>{
+    res.send(results.data);
+  })
+  .catch(err => res.status(500).send(err));
+});
+
+router.get("/hours/:week", (req, res) => {
+  db(`SELECT * FROM schedule WHERE week=${req.params.week};`) 
+  .then(results =>{
+    res.send(results.data);
+  })
+  .catch(err => res.status(500).send(err));
+});
+
+router.get("/hours/:week/:employee", (req, res) => {
+  db(`SELECT * FROM schedule WHERE week=${req.params.week} && employee="${req.params.employee}";`) 
+  .then(results =>{
+    res.send(results.data);
+  })
+  .catch(err => res.status(500).send(err));
+});
+
+router.post("/hours", (req, res) => {
+  const {week, employee, start, finish, hour, day, date } = req.body;
+  db(`INSERT into schedule (week, employee, start, finish, hour, day, date) VALUES("${week}", "${employee}","${start}","${finish}","${hour}","${day}","${date}")`)
+  .then(results =>{
+    res.send(results.data);
+  })
+  .catch(err => res.status(500).send(err));
+});
+
+router.delete("/hours/:id", async (req, res) => {
+  const weekId = await db(`SELECT week FROM schedule WHERE id=${req.params.id};`)
+//  (results => { // reutun id from that week
+//    JSON.stringify(results)}) 
+const results = JSON.stringify(weekId.data);
+console.log(weekId.data, results)
+ await 
+  db(`DELETE FROM schedule WHERE id=${req.params.id};`)// return 
+  await db(`SElECT * FROM schedule WHERE  week="${results[0]["week"]}";`)
+  .then(result =>{
+    res.send(result.data);
+  })
+});
 module.exports = router;
