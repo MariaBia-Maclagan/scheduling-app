ef6f8b4adf50668ca6e89c0b9fd35668a05ae6a8
MariaBia-Maclagan <82051716+MariaBia-Maclagan@users.noreply.github.com>
2022-05-23 20:27:59 +0100

    update

--- README.md
+++ README.md
@@ -1,11 +1,14 @@
 # scheduling-app
 
-The scheduling app is for a store manager to organise staff shifts throughout the week from Sunday (Sun) to Saturday (Sat). From the admin component the user will be able to post each employee time for each day of the week in sequence. From the Adminview component the user can delete individual staff times if needed and view schedule for an specific week.
-Other users should be able to only view the schedule times once posted in a separated user view.
+The scheduling app is for a store manager to organise staff shifts throughout the week from Sunday (Sun) to Saturday (Sat). From the Admin component the user will be able to POST each employee time for each day of the week in sequence. From the Adminview component the user can DELETE individual staff times if needed and view schedule for an specific week.
+Other users should be able to only view the schedule times once posted in a separated user view component.
 
 # To run the app
 
 1- Create the database in the MySQL CLI, type `create database hours;` 
+3- create `.env` file 
+DB_NAME = hours
+DB_PASS= YOUR_PASSWORD
 2- Replace `YOUR_PASSWORD` with your actual password on `.env` file
 3- Run `node model/database.js` in your **TERMINAL** to create table schedule
 
--- client/src/User.js
+++ client/src/User.js
@@ -3,36 +3,7 @@ import './App.css';
 
 
 export default function User(){
-    let [weekInput, setWeekInput] = useState ("");
-    const [weekSchedule, setWeekSchedule] = useState([]);
-    
-        const handleUserSearch= (event) => {
-           
-            let weekNumber = Number(event.target.value);
-            setWeekInput(weekNumber)
-            console.log(weekInput)
-            // console.log(typeof weekInput)
-        };
-        
-        const handleUserGet=(event) =>{
-            //event.preventDefault();
-            getWeekSchedule(event);
-            setWeekInput("");
-        }
-        const getWeekSchedule =(week) => {
-            fetch(`/hours/${week}`)
-            .then(res => res.json())
-            .then( response => {
-                setWeekSchedule(response);
-              
-            
-            })
-            
-            .catch(e => console.log(e));
-        }
-    
-      
-    
+  
     return(
         <div >
         <hr className="form"/>
@@ -41,33 +12,17 @@ export default function User(){
        
                 <div className="input-group-text">Week</div>
                 <input className="form-control"
-                onChange={handleUserSearch} 
-                value={weekInput}
+                onChange=""
+                value=" "
                 type="number" 
                 name="week" 
                 placeholder="1" />
-        <button className="btn btn-primary" type="submit" onClick={handleUserGet}> View Schedule </button>
+        <button className="btn btn-primary" type="submit" onClick=" "> View Schedule </button>
         </div>
         </div>
-        <div className="container rounded mt-5 ">
-            <div className="row ">
-           {  weekSchedule.map(hour =>{
-                    return(   
-                        <div  className="col border rounded " key={hour.id}>
-                            <div className="mb-2 border-bottom"><strong>{hour.day}</strong> </div>
-                            <div className="mb-1">{hour.employee}<br/></div>
-                            <div><strong>Start:</strong> {hour.start} <br/> <strong>Finish:</strong> {hour.finish} </div>
-                            <div className="mb-3"><strong>Hrs:</strong> {hour.hour}</div>
-                            </div>
-                            
-                    );
-                })}
-
-       
-
+    
 </div>
-       </div>
-       </div>
+       
        
     );
 }
